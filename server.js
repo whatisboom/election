111c60d696f54776d938e83b75ed765a15eee1ca
Brandon Jernigan <brandon@whatisboom.com>
2015-10-19 21:28:44 -0500

    added server file for index.html hosting

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8888;
+
+var router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router.use(function(request, response, next) {
+    console.log('%s %s %s', request.method, request.url, request.path);
+    next();
+});
+
+app.use('*', function(request, response) {
+    response.sendfile('./index.html');
+});
+
+app.listen(port);
